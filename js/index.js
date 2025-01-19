50d3a9d14f598ded6200cf22b8363af1e45d85b8
David Joslin <davidjoslin1990@gmail.com>
2025-01-19 01:08:45 -0500

    updated js index and added answers.txt

--- js/index.js
+++ js/index.js
@@ -1,4 +1,25 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+//Arithmetic Operators
+console.log(10+5);
+console.log(10-5);
+console.log(10*5);
+console.log(10/5);
+console.log(10%5);
 
+//Comparison Operators
+console.log(1==1);
+console.log(1=="1");
+console.log(1==="1");
+console.log(1!=1);
+console.log(1!=="1");
+console.log(1<2);
+console.log(1>2);
+console.log(2>=2);
+console.log(2<=2);
+
+//Logical Operators
+console.log(1||2);
+console.log(2||1);
+console.log(1&&1);
